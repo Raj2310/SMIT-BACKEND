5da5af11b0044943290b3aa26c6b947dbfa2959c
raj vaibhav singh <rajvaibhavsingh2310@gmail.com>
2017-03-31 20:08:37 +0530

    fb1

--- index.js
+++ index.js
@@ -24,12 +24,20 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === '1staprilwtf') {
-        res.send(req.query['hub.challenge'])
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-    res.send('Error, wrong token')
+    res.sendStatus(200)
 })
+
+var token = "EAARedKwegwoBAPOFQcZBshYmk1FS60zmyXxtTELhhPvYsTo6UC3dab8BSJ9HAZBH3ERnIZAM7ciVs8XFwr5SO1JaY3dlpZBRgkymLSFsrw0As6YI2X3XmZBMEXSrbStuwFrcUeORVRo1HCpVjrNBYGhpeLddEN2mjEkCd3ERJlwZDZD";
 let port = process.env.PORT || 5000;
 //init Express Router
 connection.connect();
@@ -41,3 +49,25 @@ app.use('/method',method);
 //start the Express server
 app.listen(port);
 console.log('Listening on port ' + port);
+
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
